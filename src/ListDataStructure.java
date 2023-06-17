79393b5cd2f16a15a8d2671cb1549b8a3fcc8517
Patrick Normile <43999718+Patnor@users.noreply.github.com>
2023-06-16 21:35:01 -0700

    Added expandArray using generics

--- src/ListDataStructure.java
+++ src/ListDataStructure.java
@@ -1,18 +1,65 @@
+import java.util.Arrays;
+
 public class ListDataStructure {
 
-    //create javadoc comments for the expandArray method
     /**
      * This method takes an array and doubles its size
+     * 
      * @param arr the array to be doubled
      * @return the doubled array
      */
-    public int[] expandArray(int[] arr) {
-        
+    public static int[] expandArray(int[] arr) {
+
         int[] newArr = new int[arr.length * 2];
         for (int i = 0; i < arr.length; i++) {
             newArr[i] = arr[i];
         }
-        
+
         return newArr;
     }
+
+    /**
+     * This method takes an array and doubles its size
+     * Uses Arrays.copyOf
+     * 
+     * @param arr the array to be doubled
+     * @return the doubled array
+     */
+    public static <T> T[] expandArray(T[] arr) {
+        T[] newArr = Arrays.copyOf(arr, arr.length * 2);
+        return newArr;
+    }
+
+    /**
+     * This method takes an array and doubles its size
+     * Uses System.arraycopy
+     * 
+     * @param arr the array to be doubled
+     * @return the doubled array
+     */
+    public static <T> T[] expandArray2(T[] arr) {
+        int newLength = arr.length * 2;
+        T[] newArr = (T[]) new Object[newLength];
+        System.arraycopy(arr, 0, newArr, 0, arr.length);
+        return newArr;
+    }
+
+    /**
+     * This method takes an array and doubles its size
+     * uses a for loop
+     * 
+     * @param arr the array to be doubled
+     * @return the doubled array
+     */
+    public static <T> T[] expandArray3(T[] arr) {
+        int newLength = arr.length * 2;
+        T[] newArr = (T[]) new Object[newLength];
+
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+
+        return newArr;
+    }
+
 }
