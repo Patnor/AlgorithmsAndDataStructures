0328a4a5ad9906e72b1db75ea01f3eeacebe39db
Patrick Normile <43999718+Patnor@users.noreply.github.com>
2023-06-18 16:39:35 -0700

    created MyArrayList.java as an example

--- src/ListDataStructure.java
+++ src/ListDataStructure.java
@@ -1,6 +1,16 @@
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
-public class ListDataStructure {
+public class ListDataStructure <T> implements Iterable<T> {
+
+    private int theSize = 0;
+    private T[] arr;
+
+    public int size() {
+        return theSize;
+    }
 
     /**
      * This method takes an array and doubles its size
@@ -62,4 +72,75 @@ public class ListDataStructure {
         return newArr;
     }
 
+    /**
+     * This method takes an array and doubles its size
+     * uses a for each loop
+     * 
+     * @param arr the array to be doubled
+     * @return the doubled array
+     */
+    public static <T> void print(T[] arr) {
+        for (T element : arr) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+   
+    /**
+     * This method prints the elements of a collection
+     * This is an example using an iterator.
+     * 
+     * @param arr the collection to be printed
+     */
+    public static <T> void printIterator(Collection<T> arr) {
+        Iterator<T> it = arr.iterator();
+        while (it.hasNext()) {
+            System.out.print(it.next() + " ");
+        }
+    }
+
+    
+    /**
+     * This method removes all even numbers from a list
+     * Quadratic time complexity for ArrayList
+     * linear time complexity for LinkedList
+     * 
+     * @param list the list to be modified
+     */
+    public static void removeEvens(List<Integer> list){
+        Iterator<Integer> it = list.iterator();
+        while (it.hasNext()) {
+            if (it.next() % 2 == 0) {
+                it.remove();
+            }
+        }
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+       return new ArrayListIterator();
+    }
+
+    private class ArrayListIterator implements Iterator<T> {
+
+        private int currentIndex = 0;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex < size();
+        }
+
+        @Override
+        public T next() {
+            //TODO: check if there is a next element
+            return (T) arr[currentIndex++];
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
 }
---
+++ src/MyArrayList.java
@@ -0,0 +1,136 @@
+import java.util.Iterator;
+
+/**
+ * This class creates an ArrayList. It is an example taken from the book
+ * Data Structures and Algorithms in Java by Mark Allen Weiss.
+ * 
+ * @param <T> the type of the ArrayList
+ */
+public class MyArrayList<T> implements Iterable<T> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+    private int theSize;
+    private T[] theItems;
+
+    public MyArrayList() {
+        doClear();
+    }
+
+    public void clear() {
+        doClear();
+    }
+
+    private void doClear() {
+        theSize = 0;
+        ensureCapacity(DEFAULT_CAPACITY);
+    }
+
+    public int size() {
+        return theSize;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public void trimToSize() {
+        ensureCapacity(size());
+    }
+
+    public T get(int idx) {
+        if (idx < 0 || idx >= size()) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return theItems[idx];
+    }
+
+    public T set(int idx, T newVal) {
+        if (idx < 0 || idx >= size()) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        T old = theItems[idx];
+        theItems[idx] = newVal;
+        return old;
+    }
+
+    public void ensureCapacity(int newCapacity) {
+        if (newCapacity < theSize) {
+            return;
+        }
+        T[] old = theItems;
+        theItems = (T[]) new Object[newCapacity];
+        for (int i = 0; i < size(); i++) {
+            theItems[i] = old[i];
+        }
+    }
+
+    public boolean add(T x) {
+        add(size(), x);
+        return true;
+    }
+
+    /**
+     * Adds an element to the ArrayList
+     * 
+     * @param idx the index to add the element
+     * @param x   the element to add
+     */
+    public void add(int idx, T x) {
+        if (theItems.length == size()) {
+            // double the capacity of the ArrayList if it is full. The +1
+            // is to avoid an infinite loop if the capacity is 0.
+            ensureCapacity(size() * 2 + 1);
+        }
+        for (int i = theSize; i > idx; i--) {
+            theItems[i] = theItems[i - 1];
+        }
+        theItems[idx] = x;
+        theSize++;
+    }
+
+    public T remove(int idx) {
+        T removedItem = theItems[idx];
+        for (int i = idx; i < size() - 1; i++) {
+            theItems[i] = theItems[i + 1];
+        }
+        theSize--;
+        return removedItem;
+    }
+
+    /**
+     * This method creates an iterator for the ArrayList class.
+     * 
+     * @return an iterator for the ArrayList class
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayListIterator();
+    }
+
+    /**
+     * This class creates an iterator for the ArrayList class.
+     */
+    private class ArrayListIterator implements Iterator<T> {
+
+        private int current = 0;
+
+        @Override
+        public boolean hasNext() {
+            return current < size();
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new java.util.NoSuchElementException();
+            }
+            return theItems[current++];
+        }
+
+        @Override
+        public void remove() {
+            MyArrayList.this.remove(--current);
+        }
+    }
+
+}
